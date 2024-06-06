7d389d2ea64676bb590a09625cfd52e86fc91e2e
onixapple <radu.cazacu1@gmail.com>
2024-06-06 15:50:23 +0200

    finished sidebar, the most tough of all

--- YTClone/src/layouts/PageHeader.tsx
+++ YTClone/src/layouts/PageHeader.tsx
@@ -1,22 +1,17 @@
 import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react"
 import logo from "../assets/Logo.png"
 import { Button } from "../components/Button"
-import { useCallback, useState } from "react"
+import { useState } from "react"
+import { useSidebarContext } from "../context/SidebarContext"
 
 export function PageHeader() {
 
     const [showFullWidthSearch, setShowFullWidthSearch] = useState(false)
+    const {toggle} = useSidebarContext()
 
     return (
         <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-            <div className={`flex gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? "hidden" : "flex"}`}>
-                <Button variant="ghost" size="icon">
-                    <Menu />
-                </Button>
-                <a href="/">
-                    <img src={logo} alt="none" className="h-6" />
-                </a>
-            </div>
+            <PageHeaderFirstSection hidden={showFullWidthSearch}/>
             <form className={`md:flex gap-4 flex-grow justify-center ${showFullWidthSearch ? "flex" : "hidden md:flex"}`}>
                 {showFullWidthSearch && <Button onClick={() => setShowFullWidthSearch(false)} type="button" size="icon" variant="ghost" className="flex-shrink-0">
                     <ArrowLeft />
@@ -55,4 +50,21 @@ export function PageHeader() {
             </div>
         </div>
     )
+}
+
+type PageHeaderFirstSectionProps = {
+    hidden?: boolean
+}
+
+export function PageHeaderFirstSection({hidden=false}: PageHeaderFirstSectionProps) {
+    const {toggle} = useSidebarContext()
+
+    return  <div className={`flex gap-4 items-center flex-shrink-0 ${hidden ? "hidden" : "flex"}`}>
+    <Button onClick={toggle} variant="ghost" size="icon">
+        <Menu />
+    </Button>
+    <a href="/">
+        <img src={logo} alt="none" className="h-6" />
+    </a>
+</div>
 }
